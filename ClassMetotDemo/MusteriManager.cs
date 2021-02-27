dbe31d0c3bc26f905bf13c71701166fd9e06e58d
Enes KARAGÖZ <eneskaragoz44@gmail.com>
2021-02-27 18:31:07 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri Eklendi!");
+            Console.WriteLine("Adı: " + musteri.Ad);
+            Console.WriteLine("Soyadı: " + musteri.Soyad);
+        }
+        public void MusteriListele(Musteri[] musteriler) 
+        {
+            Console.WriteLine("Müşteriler Listeleniyor!");
+
+            foreach (var musteri in musteriler) 
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " ");
+            }
+
+        }
+        public void MusteriSil(Musteri musteri) 
+        {
+            Console.WriteLine("Kaydınızı sildik" );
+            Console.WriteLine("Sayın : " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ali";
+            musteri1.Soyad = "Yılmaz";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Mehmet";
+            musteri2.Soyad = "Öztürk";
+
+            int i;
+            Console.WriteLine("Bir işlem seçiniz.");
+            Console.WriteLine("1-Müşteri Ekleme");
+            Console.WriteLine("2-Müşteri Silme");
+            
+
+            i = Convert.ToInt16(Console.ReadLine());
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+
+            if (i == 1)
+            {
+                musteriManager.MusteriEkle(musteri1);
+                musteriManager.MusteriEkle(musteri2);
+            }
+            else if (i == 2)
+            {
+                musteriManager.MusteriListele(musteriler);
+            }
+            else if (i == 3)
+            {
+                musteriManager.MusteriSil(musteri1);
+                musteriManager.MusteriSil(musteri2);
+            }
+
+        }
+    }
+}
